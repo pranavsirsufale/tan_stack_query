a17c60e201635ef37829ccf462c75beacac239c5
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-14 12:17:56 +0530

    add code

--- tanstackquery/src/App.jsx
+++ tanstackquery/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter } from 'react-router-dom';
+import { MainLayout } from './components/Layout/MainLayout';
+import { Home } from './pages/Home'
+import { FetchData } from './pages/FetchData'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <MainLayout /> ,
+    children : [
+      {
+        path : '/',
+        element : <Home/>,
+
+      },
+      {
+        path : "/tred",
+        element : <FetchData/>
+      },
+      {
+        path : '/rq',
+        element :  <FetchRQ/>
+      }
+    ]
+  }
+])
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hi there !I'm Pranav Sirsufale </h1>
     </>
   )
 }
---
+++ tanstackquery/src/pages/FetchData.jsx
@@ -0,0 +1,10 @@
+
+
+export const FetchData = () => {
+    return (
+        <>
+        <h1> Fetch Data</h1>
+        </>
+    )
+}
+
