6fbb6f11051713ec681c4eea23e74e63e2fac8d8
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-15 16:19:27 +0530

    add a problem

--- tanstackquery/src/pages/FetchRQ.jsx
+++ tanstackquery/src/pages/FetchRQ.jsx
@@ -1,12 +1,35 @@
-
+import { fetchPosts } from "../API/Api";
+import { useState, useEffect } from "react";
 
 export const FetchRQ = () => {
-    return (
-        <>
-            <h1>
-                Fetch RQ Page 
-            </h1>
-        </>
-    )
-}
+  const [posts, setPosts] = useState([]);
 
+  const getPostData = async () => {
+    try {
+      const res = await fetchPosts();
+      setPosts(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPostData();
+  }, []);
+
+  !posts ?? <h1> Loading... </h1>;
+
+  return (
+    <>
+      <h1>
+        <ul>
+          {posts.map((current) => {
+            const { title, body, id } = current;
+
+            return <li key={id} >{title}</li>;
+          })}
+        </ul>
+      </h1>
+    </>
+  );
+};
