362815e55e0b4bc6c9bd8a3769e295d49a4d05fc
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-23 12:46:24 +0530

    add code

--- vote_app/server.js
+++ vote_app/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const app = express()
-import dotenv from 'dotenv'
-dotenv = dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv = dotenv.config()
 import bodyParser from 'body-parser'
 
 
