4bf61bf4f3274e40e6f5b38cc9b4b55787d446c0
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-23 12:40:37 +0530

    add code

---
+++ vote_app/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+const app = express()
+import dotenv from 'dotenv'
+dotenv = dotenv.config()
+import bodyParser from 'body-parser'
+
+
+
+
+
+
+app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log('listening on port 3000')
+})
+
+
