ddef1f71f3a2ef08f063117e638d0204ddc75fe5
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-26 22:28:08 +0530

    add backend logic and code

--- back/db/db.js
+++ back/db/db.js
@@ -5,21 +5,16 @@ const connectDB = async () =>{
     mongoose.connect(`${process.env.DB_URI}${process.env.DB_NAME}`)
 
 const db = mongoose.connection;
-
 db.on('connected', (host)=>{
     console.log('DATABASE HAS BEEN CONNECTED SUCCESSFULLY :::: ')
     console.log(db.host)
 })
-
 db.on('error',(error)=>{
     console.log(error)
 })
-
 db.on('disconnected',()=>{
     console.log('DB HAS BEEN DISCONNECTED ::: ')
-})
-
-}
+})}
 
 
 export { connectDB }
