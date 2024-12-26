39fc68d238b08f5f995a9d00803ca09c0f40588c
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-26 23:20:22 +0530

    add backend logic and code

--- back/app.js
+++ back/app.js
@@ -1,7 +1,35 @@
 import express from 'express'
 import 'dotenv/config'
 import { connectDB } from './db/db.js'
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 const app = express()
+
+const limit = '16kb'
+
+
+// cors proxy for 
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+
+// accespting the json data with it's limit
+app.use(express.json({
+    limit
+}))
+
+
+app.use(express.urlencoded({extended : true, limit }))
+
+app.use(express.static('public'))
+
+
+
+
+
 const PORT = process.env.PORT || 3000;
 
 
