6e086764bbe9ec2dd20fc1a3679724de20f80d4c
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-28 00:11:54 +0530

    add backend logic and notes

--- back/controllers/user.controller.js
+++ back/controllers/user.controller.js
@@ -3,9 +3,18 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 
 const registerUser = asyncHandler(async (req,res)=>{
     
+   const { userName , email , password } = req.body;
+
+      
+    
+ 
+     
+    console.log(req.body)
+
     res.status(200)
     .json({
-        message : 'Ok'
+        message : 'Ok',
+        
     })
 })
 
