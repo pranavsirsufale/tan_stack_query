02cceeeddc0794089208604fa36c37d2790ecb18
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-27 18:57:36 +0530

    add backend logic and notes

--- back/utils/cloudinary.js
+++ back/utils/cloudinary.js
@@ -1,15 +1,27 @@
-import {v2 as cloudinary} from 'cloudinary';
-import fs from 'fs'
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
-  
-cloudinary.config( {
-    cloud_name: process.env.CLOUDINARY_NAME, 
-    api_key :process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
-}
-)
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+        //? file has been uploaded successfully
+        return response;
+    } catch (error) {
+        console.log("ERROR OCCURED WHILE UPLOADING ON CLOUDINARY ::: ", error);
+        // remove the locally saved file as the uplod operaiton done
+        fs.unlinkSync(localFilePath);
+    } finally {
+        fs.unlinkSync(localFilePath);
+    }
+};
 
-
-
-
+export { uploadOnCloudinary };
