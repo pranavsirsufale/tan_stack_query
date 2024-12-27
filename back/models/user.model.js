351fa3414c4b0533692fc1dd01ea4bc7adad1fb4
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-27 08:57:42 +0530

    add backend logic

--- back/models/user.model.js
+++ back/models/user.model.js
@@ -9,6 +9,38 @@ const userSchema = new Schema(
             lowerCase : true,
             trim : true,
             index : true
+        },
+        email : {
+            type : String,
+            required : true,
+            unique : true,
+            lowerCase : true,
+            trim : true
+        },
+        fullName : {
+            type : String,
+            required : true,
+            trim : true
+        },
+        avatar : {
+            type : String, // cloudinary url
+            required : true,
+
+        },
+        coverImage : {
+            type : String
+        },
+        watchHistory : [{
+            type : Schema.Types.ObjectId ,
+            ref : 'Video'
+            }
+        ],
+        password : {
+            type : String,
+            required : [true, 'Password is required']
+        },
+        refreshToken : {
+            type : String,
         }
     },
     {
